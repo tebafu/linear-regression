0bbc180b3806705fded4bc1ba5cbd82ba283f342
Napoleon-Gartsonis <napoleon.gartsonis@gmail.com>
2022-02-03 22:45:21 +0200

    Added sigmoid function

--- linear_regression.py
+++ linear_regression.py
@@ -12,8 +12,22 @@ class LinearRegression:
     def initialize_weights(self):
         pass
 
-    def sigmoid(self):
-        pass
+    # noinspection PyMethodMayBeStatic
+    def sigmoid(self, x):
+        """
+        Sigmoid function : 1/(1+e^-x)
+
+        Parameters
+        ----------
+        x : float or nd.array
+            input
+        Returns
+        -------
+        float or nd.array
+            the result of the function applied to the input
+        """
+        s = 1.0 / (1.0 + np.exp(-x))
+        return s
 
     def compute_cost(self):
         pass
