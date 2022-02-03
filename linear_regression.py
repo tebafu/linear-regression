476b57f28579e8c3c5dead88b2b0ee25c9c72f08
Napoleon-Gartsonis <napoleon.gartsonis@gmail.com>
2022-02-03 22:43:24 +0200

    First commit with the model methods

---
+++ linear_regression.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+class LinearRegression:
+    """
+    A linear regression model
+
+    """
+    def __init__(self):
+        pass
+
+    def initialize_weights(self):
+        pass
+
+    def sigmoid(self):
+        pass
+
+    def compute_cost(self):
+        pass
+
+    def gradient_descent(self):
+        pass
+
+    def update_weights(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
